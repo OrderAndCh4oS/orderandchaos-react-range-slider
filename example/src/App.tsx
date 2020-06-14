8d8f0c4cdf5571ed500d1feffd2c429dc0826b59
Sarcoma <sarcoma@live.co.uk>
2020-06-14 14:24:11 +0100

    first commit

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-import { ExampleComponent } from 'orderandchaos-react-range-slider'
+import { RangeSlider, RangeSliderTypes } from 'orderandchaos-react-range-slider'
 import 'orderandchaos-react-range-slider/dist/index.css'
 
 const App = () => {
-  return <ExampleComponent text="Create React Library Example 😄" />
+  const [linearValue, setLinearValue] = useState<number>(0)
+  const [logValue, setLogValue] = useState<number>(100)
+  return (
+    <>
+      <input readOnly={true} value={linearValue}/>
+      <RangeSlider value={linearValue} setValue={setLinearValue} min={0} max={100} step={1}/>
+      <input readOnly={true} value={logValue}/>
+      <RangeSlider value={logValue} setValue={setLogValue} type={RangeSliderTypes.LOG} min={100} max={100000} decimalPlaces={0}/>
+    </>
+  )
 }
 
 export default App
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,135 @@
 import * as React from 'react'
+import { ChangeEvent, FC, FocusEvent } from 'react'
 import styles from './styles.module.css'
 
-interface Props {
-  text: string
+export const logScale = (value: number, max: number, min: number = 1): number => {
+  if(value === 0) return min;
+
+  const minP = 1
+  const maxP = 1000
+
+  const minV = Math.log(min)
+  const maxV = Math.log(max)
+
+  const scale = (maxV - minV) / (maxP - minP)
+
+  const result = Math.exp(minV + scale * (value - minP))
+  console.log('log', value, max, min, minV, maxV, scale)
+  console.log('log', result)
+  return result
 }
 
-export const ExampleComponent = ({ text }: Props) => {
-  return <div className={styles.test}>Example Component: {text}</div>
+export const inverseLogScale = (lg: number, max: number, min: number = 1): number => {
+  if(lg === 0) return min;
+
+  const minP = 1
+  const maxP = 1000
+
+  const minV = Math.log(min)
+  const maxV = Math.log(max)
+
+  const scale = (maxV - minV) / (maxP - minP)
+  const result = (Math.log(lg) - minV) / scale + minP
+  console.log('inverse', lg, max, min, minV, maxV, scale)
+  console.log('inverse', result)
+  return result
+}
+
+const getMin = (type: RangeSliderTypes, value: number): number => {
+  switch (type) {
+    case RangeSliderTypes.LINEAR:
+      return value
+    case RangeSliderTypes.LOG:
+      return 0
+  }
+}
+
+const getMax = (type: RangeSliderTypes, value: number): number => {
+  switch (type) {
+    case RangeSliderTypes.LINEAR:
+      return value
+    case RangeSliderTypes.LOG:
+      return 1000
+  }
+}
+
+const getValue = (type: RangeSliderTypes, value: number, max: number, min: number = 0): number => {
+  switch (type) {
+    case RangeSliderTypes.LINEAR:
+      return value
+    case RangeSliderTypes.LOG:
+      return inverseLogScale(value, max, min)
+  }
+}
+
+const toDecimalPlaces = (value: number, decimalPlaces: number, base = 10): number => {
+  const pow = Math.pow(base||10, decimalPlaces);
+  return Math.round(value*pow) / pow;
+}
+
+export enum RangeSliderTypes {
+  LOG,
+  LINEAR
+}
+
+interface Props {
+  step?: number,
+  min?: number,
+  max?: number,
+  value?: number,
+  type?: RangeSliderTypes,
+  showTab?: boolean,
+  decimalPlaces?: number
+  setValue: (value: number) => void,
+}
+
+const handleChange = (type: RangeSliderTypes, setValue: (value: number) => void, decimalPlaces: number, max: number, min: number) => (e: ChangeEvent<HTMLInputElement>) => {
+  switch (type) {
+    case RangeSliderTypes.LINEAR:
+      setValue(toDecimalPlaces(Number(e.target.value), decimalPlaces))
+      break
+    case RangeSliderTypes.LOG:
+      setValue(toDecimalPlaces(logScale(Number(e.target.value), max, min), decimalPlaces))
+      break
+  }
+}
+
+const handleBlur = (type: RangeSliderTypes, setValue: (value: number) => void, max: number, min: number, decimalPlaces: number) => (e: FocusEvent<HTMLInputElement>) => {
+  switch (type) {
+    case RangeSliderTypes.LINEAR:
+      setValue(Number(e.target.value))
+      break
+    case RangeSliderTypes.LOG:
+      setValue(toDecimalPlaces(logScale(Number(e.target.value), max, min), decimalPlaces))
+      break
+  }
+}
+
+export const RangeSlider: FC<Props> = (
+  {
+    setValue,
+    type = RangeSliderTypes.LINEAR,
+    showTab = true,
+    step = 1,
+    min = 0,
+    max = 100,
+    value = 0,
+    decimalPlaces = 3,
+    ...rest
+  }) => {
+  return (
+    <div className={styles.rangeSlider_wrapper}>
+      {showTab && <span className={styles.rangeSlider_tab}/>}
+      <input
+        type="range"
+        className={styles.rangeSlider}
+        onChange={handleChange(type, setValue, decimalPlaces, max, min)}
+        onBlur={handleBlur(type, setValue, max, min, decimalPlaces)}
+        value={getValue(type, value, max, min).toFixed(decimalPlaces)}
+        min={getMin(type, min)}
+        max={getMax(type, max)}
+        {...rest}
+      />
+    </div>
+  )
 }
